489184f5afd92791c81e27a9cd7ef5e513ada7f0
Nathan Santos <79227411+NathanSnt@users.noreply.github.com>
2023-03-23 13:33:26 -0300

    Nathan

--- alertrem/sistemaAlertrem/Form1.Designer.cs
+++ alertrem/sistemaAlertrem/Form1.Designer.cs
@@ -29,13 +29,34 @@ namespace sistemaAlertrem
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(263, 158);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(154, 13);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "NATHAN PASSOU POR AQUI";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblNome);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblNome;
     }
 }
 
