1b28c5e94f701c37d1ff5d84ba935e2d608e199a
João lameiro <joao.vldutra@senacsp.edu.br>
2023-03-24 15:26:58 -0300

    atua

---
+++ alertrem/sistemaAlertrem/Frmcomentario.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace sistemaAlertrem
+{
+    public partial class Frmcomentario : Form
+    {
+        public Frmcomentario()
+        {
+            InitializeComponent();
+            TestarConexaoBanco();
+        }
+
+        private void Frmcomentario_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TestarConexaoBanco();
+            
+        }
+        public void TestarConexaoBanco()
+        {
+            bool bancoOnline = Conexao.testarConexao();
+            if (bancoOnline)
+            {
+                lblEstadoDoBanco.Text = "Online";
+                lblEstadoDoBanco.ForeColor = Color.Green;
+            }
+            else
+            {
+                lblEstadoDoBanco.Text = "Offline";
+                lblEstadoDoBanco.ForeColor = Color.Red;
+            }
+        }
+    }
+
+}
--- alertrem/sistemaAlertrem/Program.cs
+++ alertrem/sistemaAlertrem/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace sistemaAlertrem
 {
@@ -16,7 +17,8 @@ namespace sistemaAlertrem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Frmcomentario());
         }
     }
 }
+
